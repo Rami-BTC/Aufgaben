0dd66edd51af1d0f05444cc97b6238aabf9b6425
Abdullatif <rami.abdullatif@btc-ag.com>
2024-09-17 13:56:17 +0200

    DecimalToBinary Upload 2

--- src/main/java/com/btcag/bootcamp/DecimalToBinary.java
+++ src/main/java/com/btcag/bootcamp/DecimalToBinary.java
@@ -1,4 +1,23 @@
 package com.btcag.bootcamp;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Gebe eine positive Ganze Zahl ein: ");
+
+        int decimal = scanner.nextInt();
+
+        String result = "";
+
+        while (decimal > 0) {
+            int resultModulo = decimal % 2;
+            result = resultModulo + result;
+            decimal = decimal / 2;
+
+            System.out.print(result);
+        }
+    }
 }
+
